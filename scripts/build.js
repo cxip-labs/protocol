90228d76d76833f15252150e315bdf6902d12fa1
Vitto <thisisvitto@gmail.com>
2022-02-10 22:38:41 +0100

    Update build.js
    Loading correct config environment variables now

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 const buildDir = './src';
 const deployDir = './contracts';
 
-const NETWORK = process.env.NETWORK || 'local';
+const { NETWORK } = require('../config/env');
+
 const buildConfig = JSON.parse(
   fs.readFileSync('./config/build.config.json', 'utf8')
 );
