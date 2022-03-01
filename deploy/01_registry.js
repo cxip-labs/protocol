6693b0148ca9181321fe77a2d1fd6ec30a153ed6
Alexander <alexanderattar@gmail.com>
2022-03-01 14:40:09 -0500

    Inject identity and erc721 proxy bytecode

--- deploy/01_registry.js
+++ deploy/01_registry.js
@@ -1,3 +1,5 @@
+const hre = require('hardhat');
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -5,14 +7,13 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const fs = require('fs');
   const buildDir = './src';
   const deployDir = './contracts';
-
-  const { NETWORK } = require('../config/env');
-
   const buildConfig = JSON.parse(
     fs.readFileSync('./config/build.config.json', 'utf8')
   );
+
+  const network = hre.network.name;
   const config = JSON.parse(
-    fs.readFileSync('./config/' + NETWORK + '.config.json', 'utf8')
+    fs.readFileSync('./config/' + network + '.config.json', 'utf8')
   );
 
   await deploy('CxipRegistry', {
@@ -21,57 +22,72 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     log: true,
   });
 
+  // This is a bit of a hack to inject the correct registry address and bytecode params into the build config.
   const cxipRegistry = await ethers.getContract('CxipRegistry');
   config.registry = cxipRegistry.address;
+  const cxipIdentityProxy = await hre.deployments.getArtifact(
+    'CxipIdentityProxy'
+  );
+  config.identityProxyBytecode = cxipIdentityProxy.bytecode;
 
-  const replaceValues = function (data) {
-    Object.keys(buildConfig).forEach(function (key, index) {
-      data = data.replace(new RegExp(buildConfig[key], 'gi'), config[key]);
-    });
-    return data;
-  };
+  const erc721Proxy = await hre.deployments.getArtifact('CxipERC721Proxy');
+  config.erc721ProxyBytecode = erc721Proxy.bytecode;
 
-  const recursiveBuild = function (buildDir, deployDir) {
-    fs.readdir(buildDir, function (err, files) {
-      if (err) {
-        throw err;
-      }
-      files.forEach(function (file) {
-        fs.stat(buildDir + '/' + file, function (err, stats) {
-          if (err) {
-            throw err;
-          }
-          if (stats.isDirectory()) {
-            // we go into it
-            fs.mkdir(deployDir + '/' + file, function () {
-              recursiveBuild(buildDir + '/' + file, deployDir + '/' + file);
-            });
-          } else {
-            if (file.endsWith('.sol')) {
-              console.log(file);
-              fs.readFile(buildDir + '/' + file, 'utf8', function (err, data) {
-                if (err) {
-                  throw err;
-                }
-                fs.writeFile(
-                  deployDir + '/' + file,
-                  replaceValues(data),
-                  function (err) {
-                    if (err) {
-                      throw err;
-                    }
-                  }
-                );
-              });
-            }
-          }
-        });
-      });
-    });
-  };
-  fs.mkdir(deployDir, function () {
-    recursiveBuild(buildDir, deployDir);
-  });
+  console.log(`Config: ${JSON.stringify(config, null, 2)}`);
+
+  if (network === 'hardhat') {
+    console.log('Test network detected. Skipping code injection');
+    return;
+  }
+
+  // const replaceValues = function (data) {
+  //   Object.keys(buildConfig).forEach(function (key, index) {
+  //     data = data.replace(new RegExp(buildConfig[key], 'gi'), config[key]);
+  //   });
+  //   return data;
+  // };
+
+  // const recursiveBuild = function (buildDir, deployDir) {
+  //   fs.readdir(buildDir, function (err, files) {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     files.forEach(function (file) {
+  //       fs.stat(buildDir + '/' + file, function (err, stats) {
+  //         if (err) {
+  //           throw err;
+  //         }
+  //         if (stats.isDirectory()) {
+  //           // we go into it
+  //           fs.mkdir(deployDir + '/' + file, function () {
+  //             recursiveBuild(buildDir + '/' + file, deployDir + '/' + file);
+  //           });
+  //         } else {
+  //           if (file.endsWith('.sol')) {
+  //             console.log(file);
+  //             fs.readFile(buildDir + '/' + file, 'utf8', function (err, data) {
+  //               if (err) {
+  //                 throw err;
+  //               }
+  //               fs.writeFile(
+  //                 deployDir + '/' + file,
+  //                 replaceValues(data),
+  //                 function (err) {
+  //                   if (err) {
+  //                     throw err;
+  //                   }
+  //                 }
+  //               );
+  //             });
+  //           }
+  //         }
+  //       });
+  //     });
+  //   });
+  // };
+  // fs.mkdir(deployDir, function () {
+  //   recursiveBuild(buildDir, deployDir);
+  // });
 };
 
 module.exports.tags = ['CxipRegistry'];
