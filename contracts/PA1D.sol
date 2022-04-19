37061ccb7f12cafad7a037d264774dd36c83099a
Alexander <alexanderattar@gmail.com>
2022-04-19 17:05:23 -0400

    Update latest code and verify on etherscan

--- contracts/PA1D.sol
+++ contracts/PA1D.sol
@@ -90,7 +90,7 @@ contract PA1D {
      * @return The address of the top-level CXIP Registry smart contract.
      */
     function getRegistry() internal pure returns (ICxipRegistry) {
-        return ICxipRegistry(0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512);
+        return ICxipRegistry(0x415225c0d082CB195AeE69f490c218def30966da);
     }
 
     /**
