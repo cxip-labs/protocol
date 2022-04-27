a8a1e141b919364debc283e71437b1b663380269
Vitto <admin@vitto.io>
2022-04-27 16:43:52 +0200

    updates

--- deploy/01_registry.ts
+++ deploy/01_registry.ts
@@ -18,39 +18,41 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     fs.readFileSync('./config/' + network + '.config.json', 'utf8')
   );
 
-  const cxipRegistry = await deploy('CxipRegistry', {
-    from: deployer,
-    args: [],
-    log: true,
-  });
+  if (network == 'hardhat') {
+    const cxipRegistry = await deploy('CxipRegistry', {
+      from: deployer,
+      args: [],
+      log: true,
+    });
 
-  /**
-   * These next few lines are a hack to inject the correct registry address and bytecode params into the build config
-   */
-  config.registry = cxipRegistry.address;
+    /**
+     * These next few lines are a hack to inject the correct registry address and bytecode params into the build config
+     */
+    config.registry = cxipRegistry.address;
 
-  const cxipIdentityProxy = await deploy('CxipIdentityProxy', {
-    from: deployer,
-    args: [],
-    log: true,
-  });
-  config.identityProxyBytecode = cxipIdentityProxy?.bytecode?.substring(2); // remove 0x;
+    const cxipIdentityProxy = await deploy('CxipIdentityProxy', {
+      from: deployer,
+      args: [],
+      log: true,
+    });
+    config.identityProxyBytecode = cxipIdentityProxy?.bytecode?.substring(2); // remove 0x;
 
-  const erc721Proxy = await deploy('CxipERC721Proxy', {
-    from: deployer,
-    args: [],
-    log: true,
-  });
-  config.erc721ProxyBytecode = erc721Proxy?.bytecode?.substring(2); // remove 0x
+    const erc721Proxy = await deploy('CxipERC721Proxy', {
+      from: deployer,
+      args: [],
+      log: true,
+    });
+    config.erc721ProxyBytecode = erc721Proxy?.bytecode?.substring(2); // remove 0x
 
-  fs.writeFileSync(
-    './config/' + network + '.config.json',
-    JSON.stringify(config, null, 2),
-    'utf8'
-  );
-  /**
-   * End hack
-   */
+    fs.writeFileSync(
+      './config/' + network + '.config.json',
+      JSON.stringify(config, null, 2),
+      'utf8'
+    );
+    /**
+     * End hack
+     */
+  }
 
   console.log(`Config: ${JSON.stringify(config, null, 2)}`);
   // TODO: It turns out we also need to inject the proper registry and bytecode to run tests locally
